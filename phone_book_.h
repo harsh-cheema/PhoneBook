4b50a8a629e2c2af587ed56e1f5ebc1f3efbf93a
Harsh Cheema <98507707+harsh-cheema@users.noreply.github.com>
2023-09-01 23:03:17 +0530

    Update phone_book_.h

--- phone_book_.h
+++ phone_book_.h
@@ -7,11 +7,4 @@ void display_all_contacts();
 void search_contact();
 void delete_contact();
 void update_contact();
-//void enter_details(contact *p);
-///*typedef struct contact{
-//    char name[30];
-//        char country_code[4];
-//            long int contact_no;
-//                char mail[100];
-//                }contact;*/
-//
+
