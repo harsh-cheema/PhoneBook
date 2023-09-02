eaa8e14c67ac71dd339c5710436a98bfdde3f13c
Harsh Cheema <98507707+harsh-cheema@users.noreply.github.com>
2023-09-02 14:51:52 +0530

    Add files via upload

---
+++ phone_book_.h
@@ -0,0 +1,18 @@
+typedef struct contact{
+    char name[30];
+    char country_code[4];
+    long int contact_no;
+    char mail[100];
+}contact;
+
+
+void start_phonebook();
+void sorted_insert();
+void delete_all_contacts();
+void print_choices();
+void add_contact();
+void display_all_contacts();
+void search_contact();
+void delete_contact();
+void update_contact();
+void enter_details(contact* p);
