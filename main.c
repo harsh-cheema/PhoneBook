0122a5a44483df98f650195da76a24d44df1faaf
Harsh Cheema <98507707+harsh-cheema@users.noreply.github.com>
2023-09-08 20:24:06 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
-#include <time.h>
 #include "phone_book_.h"
 
 
 int main(){
-	clock_t start_time,end_time;
-	double cpu_time_used;
-
-	start_time=clock();
+	
 
 	start_phonebook();
 
-	end_time=clock();
-	cpu_time_used=(double)((end_time-start_time)/CLOCKS_PER_SEC);
-
-	printf("CPU Time used: %f seconds\n",cpu_time_used);
+	
 }
